28f7b1e735e22679dba6ec19aa709171fe573076
Māris Muižnieks <maris.muiznieks@lumi.moe>
2023-07-13 18:37:03 +0300

    nod6 & nod7 complete

--- nod6/uzd1.py
+++ nod6/uzd1.py
@@ -1,3 +1,33 @@
-# 1.b Programma rāda gan skaitļu vidējo vērtību, gan VISUS ievadītos skaitļus
-# PS Iziešana no programmas ir ievadot "q"
+# 1.c Programma nerāda visus ievadītos skaitļus bet gan tikai TOP3 un BOTTOM3 un protams joprojām vidējo.
 
+num_list = []
+
+def num_input():
+    try:
+        n = float(input("Please input a number: "))
+        num_list.append(n)
+        pass
+    except ValueError as e:
+        print("Err...")
+        print(e)
+        exit()
+
+def average_list(lst) -> float:
+    return sum(lst) / len(lst)
+
+try:
+    size = int(input("Please input the size of the list: "))
+    pass
+except ValueError as e:
+    print("Err...")
+    print(e)
+    exit()
+
+for i in range(size):
+    num_input()
+
+num_list.sort()
+avg = average_list(num_list)
+
+print(f"The Top 3: {num_list[:3]}, The Bottom 3{num_list[-3:]}")
+print(f"The average of the list is: {avg}")
--- nod7/uzd3.py
+++ nod7/uzd3.py
@@ -1,1 +1,22 @@
 # 3. Pilsēta
+
+p0 = 1000   # Current city population
+perc = 2    # % value 0-100
+delta = 50  # Delta, yearly imigration/emigration
+p = 1200    # goal population
+
+def get_city_year(p0: int, prct: int, d: int, p1: int) -> int:
+    normalize = 0.01*prct
+    #print(normalize)
+    i = 0
+    while (p0 <= p1):
+        p0 = p0 + p0 * normalize + d
+        i += 1
+        if (p0 < 0):
+            return -1
+    return i
+
+print(get_city_year(p0, perc, delta, p))
+print(get_city_year(1000, 2, -50, 5000))
+print(get_city_year(1500, 5, 100, 5000))
+print(get_city_year(1500000, 2.5, 10000, 2000000))
